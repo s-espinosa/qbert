718ae530caf18076729224f03c41318d57b0060f
Sal Espinosa <sespinos@gmail.com>
2016-07-10 04:36:23 -0600

    Refactor animation to live in draw module

--- test/qbert_test.js
+++ test/qbert_test.js
@@ -6,25 +6,12 @@ describe('Qbert', function(){
     var qbert = new Qbert({});
 
     it('defaults to the top cube', function(){
-      assert.equal(qbert.position, 0);
+      assert.equal(qbert.currentPosition, 0);
     });
 
     it('initializes with 3 lives', function(){
       assert.equal(qbert.lives, 3);
     });
 
-  }); 
-
-  context('movement', function(){
-    var qbert = new Qbert({});
-
-    it('moves to the assigned cube', function(){
-      assert.equal(qbert.position, 0);
-      
-      qbert.move(14);
-
-      assert.equal(qbert.position, 14);
-    });
   });
-
 });
