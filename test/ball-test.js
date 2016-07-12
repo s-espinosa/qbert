cc1949d27f811f0fd65851a1e47f9913e69e3439
Kami Boers <kamiboers@gmail.com>
2016-07-11 23:42:58 -0600

    stubs working, before and after functions not working

--- test/ball-test.js
+++ test/ball-test.js
@@ -3,12 +3,10 @@ var Cube = require('../lib/cube')
 var Ball = require('../lib/ball');
 
 describe('Ball', function(){
+
   context('when initialized', function(){
-    var cube1 = new Cube({id: 0})
-    var cube2 = new Cube({id: 1})
-    var cube3 = new Cube({id: 2})
-    var board = {cubes: [cube1, cube2, cube3]}
-    var ball = new Ball({board: board});
+    var board = { cubes: [ new Cube({id: 0}), new Cube({id: 1}), new Cube({id: 2}) ] }
+    var ball  = new Ball({board: board});
 
     it('defaults to the second row of cubes', function(){
       assert.include([1,2], ball.currentPosition);
@@ -17,11 +15,12 @@ describe('Ball', function(){
     it('defaults to alive', function(){
       assert.equal(ball.alive, true);
     });
-  
+
   }); 
 
   context('death', function(){
-    var ball = new Ball({})
+    var ball  = new Ball({});
+
     it('sets alive to false when it dies', function(){
       ball.die();
       assert.equal(ball.alive, false);
